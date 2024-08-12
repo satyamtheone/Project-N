0ebffbd80a1ffa57aa0d060565d68dae8b94a3a8
Satyamsinghchauhan001 <117146497+Satyamsinghchauhan001@users.noreply.github.com>
2024-08-12 12:49:35 +0530

    .

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
+
+export default function App() {
+  return (
+    <View className=" flex-1 justify-center items-center">
+      <Text className="text-yellow-300">
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
